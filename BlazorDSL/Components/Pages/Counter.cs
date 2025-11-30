8330602dccbee6b5d6292084f77b195c0952f862
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Entwurf der Komponente für den funktionalen Ansatz (kompiliert nicht!)

--- BlazorDSL/Components/Pages/Counter.cs
+++ BlazorDSL/Components/Pages/Counter.cs
@@ -5,25 +5,25 @@ using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Web;
 
 [Route("/counter")]
-public partial class Counter : ComponentBase
+public partial class Counter : WebComponent
 {        
-    protected override void BuildRenderTree(RenderTreeBuilder __builder)
-    {
-        __builder.AddMarkupContent(0, "<h1>Counter</h1>\r\n\r\n");
-        __builder.OpenElement(1, "div");
-        __builder.AddAttribute(2, "class", "box");
-        __builder.OpenElement(3, "p");
-        __builder.AddContent(4, "Current count: ");
-        __builder.AddContent(5, currentCount);
-        __builder.CloseElement();
-        __builder.AddMarkupContent(6, "\r\n    ");
-        __builder.OpenElement(7, "button");
-        __builder.AddAttribute(8, "class", "btn btn-primary");
-        __builder.AddAttribute(9, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, IncrementCount));
-        __builder.AddContent(10, "Click me");
-        __builder.CloseElement();
-        __builder.CloseElement();
-    }
+    protected override void Render() =>
+        div(
+            h1("Counter"),
+            div(
+                attrs(
+                    className("box")
+                ),
+                p("Current count: " + currentCount),
+                button(
+                    attrs(
+                        className("btn btn-primary"),
+                        onClick(IncrementCount),
+                    ),
+                    "Click me"
+                )
+            )
+        );
 
     private int currentCount = 0;
 
---
+++ BlazorDSL/WebComponent.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDSL;
+
+public abstract class WebComponent : ComponentBase {
+    protected abstract Node Render();
+}

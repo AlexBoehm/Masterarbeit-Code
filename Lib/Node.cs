22ec7c7435b28c3f52252b3652b40fdc2c2f5d7b
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Kleine Ergänzungen

--- Lib/Node.cs
+++ Lib/Node.cs
@@ -90,9 +90,9 @@ public class ComponentNode : Node {
 }
 
 public class RenderFragmentNode : Node {
-// #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        // #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         public RenderFragment? Fragment { get; private set; }
-// #pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        // #pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 
         public RenderFragmentNode(RenderFragment? fragment) {
             Fragment = fragment;

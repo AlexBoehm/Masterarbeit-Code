cc14682b3fe3b7c4aa5d5b8678487f8e4ece92ae
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Der Inhalt der bisherigen Datei App.razor wird jetzt durch die Klasse App in der Datei App.cs über die DSL erzeugt

---
+++ BlazorDSL/Components/App.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using static BlazorDSL.Html;
+
+namespace BlazorDSL.Components;
+
+public partial class App : WebComponent
+{        
+    protected override Node Render() =>
+        Tags(
+            DocTypeHtml(),
+
+            html(
+                [lang("en")],
+                head(
+                    meta([charset("utf-8")]),
+                    meta([name("viewport"), content("width=device-width, initial-scale=1.0")]),
+                    @base([href("/")]),
+                    link([rel("stylesheet"), href("bootstrap/bootstrap.min.css")]),
+                    link([rel("stylesheet"), href("app.css")]),
+                    link([rel("stylesheet"), href("BlazorDSL.styles.css")]),
+                    link([rel("icon"), type("image/png"), href("favicon.png")]),
+                    Component<HeadOutlet>(RenderMode.InteractiveServer)
+                ),
+                body(
+                    [],
+                    Component<Routes>(RenderMode.InteractiveServer),
+                    script([src("_framework/blazor.web.js")])
+                )
+            )
+        );    
+}
--- BlazorDSL/Html.cs
+++ BlazorDSL/Html.cs
@@ -17,6 +17,21 @@ static class Html {
     public static Node button(string text) => new TagNode("button", Html.text(text));
     public static Node button(Attribute[] attributes, string text)
         => new TagNode("button", attributes, Html.text(text));
+    public static Node DocTypeHtml() => new TagNode("!DOCTYPE html");
+    public static Node html(Attribute[] attributes, params Node[] inner) => new TagNode("html", inner);
+    public static Node head(params Node[] inner) => new TagNode("div", inner);
+    public static Node meta(Attribute[] attributes, params Node[] inner)
+        => new TagNode("meta", attributes, inner);
+    public static Node @base(Attribute[] attributes, params Node[] inner)
+        => new TagNode("base", attributes, inner);
+    public static Node link(Attribute[] attributes, params Node[] inner)
+        => new TagNode("link", attributes, inner);
+    public static Node body(Attribute[] attributes, params Node[] inner)
+        => new TagNode("body", attributes, inner);
+    public static Node script(Attribute[] attributes, params Node[] inner)
+        => new TagNode("script", attributes, inner);
+
+
     #endregion
 
     public static Node Tags(params Node[] nodes)
@@ -29,8 +44,35 @@ static class Html {
 
     #region Attributes
 
-    public static Attribute className(string className)
-        => new Attribute("class", className);
+    public static Attribute className(string value)
+        => new Attribute("class", value);
+
+    public static Attribute lang(string value)
+        => new Attribute("lang", value);
+
+    public static Attribute charset(string value)
+        => new Attribute("charset", value);
+
+    public static Attribute name(string value)
+        => new Attribute("name", value);
+
+    public static Attribute content(string value)
+        => new Attribute("content", value);
+
+    public static Attribute href(string value)
+        => new Attribute("href", value);
+
+    public static Attribute link(string value)
+        => new Attribute("link", value);
+
+    public static Attribute rel(string value)
+        => new Attribute("rel", value);
+
+    public static Attribute type(string value)
+        => new Attribute("type", value);
+
+    public static Attribute src(string value)
+        => new Attribute("src", value);
 
     public static Attribute onClick(object sender, Action callback)
         => new Attribute(

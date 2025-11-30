4c7696fcf5c5c7c508cc40995ef7a36444ff9826
Alexander Böhm <alexander.boehm@alebo.de>
2025-11-30 20:57:03 +0100

    Beispiel von Linq, um nur Namen auszugeben, die mit J anfangen

--- BlazorDSL/Components/Pages/Counter.cs
+++ BlazorDSL/Components/Pages/Counter.cs
@@ -14,6 +14,7 @@ public partial class Counter : WebComponent
                 p("Current Count: " + currentCount),
                 div(
                     from name in names
+                    where name.StartsWith("J")
                     select p(name)
                 ),
                 button([
